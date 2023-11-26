161ac74bafcbc5d7c030ee36c8cf8093df516893
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2023-11-25 18:13:19 -0600

    Update imageStats.py

--- src/imageStats.py
+++ src/imageStats.py
@@ -8,9 +8,6 @@ from datetime import datetime, timezone
 ######################################################################################
 paths_ = json.load(open("%s%s" % ("00_resources/","paths.json")))
 def calc_color_moments(image):
-	# Convert image to the Lab color space
-	#lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
-	#b_channel,l_channel, a_channel = cv2.split(lab_image)
 	b_channel, g_channel, r_channel = cv2.split(image)
 	b_mean = np.mean(b_channel)
 	g_mean = np.mean(g_channel)
@@ -26,33 +23,40 @@ def calc_color_moments(image):
 	r_kurt = (np.mean((r_channel - r_mean)**4)) / r_std**4
 	return b_mean, g_mean, r_mean, b_std, g_std, r_std, b_skew, g_skew, r_skew, b_kurt, g_kurt, r_kurt;
 
-def calc_midpoint(point1, point2, point3):
-	x_mid = (point1[0] + point2[0] + point3[0]) / 3
-	y_mid = (point1[1] + point2[1] + point3[1]) / 3
-	z_mid = (point1[2] + point2[2] + point3[2]) / 3
+def calc_midpoint(point1, point2, point3, point4):
+	x_mid = (point1[0] + point2[0] + point3[0] + point4[0]) / 4
+	y_mid = (point1[1] + point2[1] + point3[1] + point4[1]) / 4
+	z_mid = (point1[2] + point2[2] + point3[2] + point4[2]) / 4
 	return x_mid, y_mid, z_mid
 
 def normailize_val(val,d_min,d_max):
 	return round(((val-d_min)/(d_max-d_min)),4)
+
+def rgb_to_hex(rgb):
+    # Ensure values are in the valid range (0 to 255)
+    r, g, b = [min(255, max(0, int(x))) for x in rgb]
+    # Convert to hexadecimal format
+    hex_color = "#{:02x}{:02x}{:02x}".format(r, g, b)
+    return hex_color
 ######################################################################################
 imageStatsRef_ = json.load(open("%s%s" % (r"02_output/","imageStats_ref.json")))
 imageStatsRef_keys = list(imageStatsRef_.keys())
 imageStats_ = {}
-poolData_ = {"mean":[],"std":[],"skew":[]}
-files_ = [f for f in listdir(paths_["content"]["images"]) if isfile(join(paths_["content"]["images"], f))]
+poolData_ = {"mean":[],"std":[],"skew":[],"kurt":[]}
+path_images = r"src/viz/"
+files_ = [f for f in listdir(path_images) if isfile(join(path_images, f))]
 for f in files_:
+	print(f)
 	parse_f  = f.split(".")
-	print(parse_f[0])
-	if parse_f[-1].lower() == "jpg" and parse_f[0] in imageStatsRef_keys:
-		date_modified = imageStatsRef_[parse_f[0]]["date_modified"]
-		impath =  "%s%s" % (paths_["content"]["images"],f)
+	if parse_f[-1].lower() == "jpg":
+		date_modified = None
+		if parse_f[0] in imageStatsRef_keys:
+			date_modified = imageStatsRef_[parse_f[0]]["date_modified"]
+		impath =  "%s%s" % (path_images,f)
 		image = cv2.imread(impath)
 		try:
 			b_mean, g_mean, r_mean, b_std, g_std, r_std, b_skew, g_skew, r_skew, b_kurt, g_kurt, r_kurt = calc_color_moments(image)
-			'''print(f"L-channel: Mean={l_mean:.2f}, Std={l_std:.2f}, Skewness={l_skewness:.2f}")
-			print(f"A-channel: Mean={a_mean:.2f}, Std={a_std:.2f}, Skewness={a_skewness:.2f}")
-			print(f"B-channel: Mean={b_mean:.2f}, Std={b_std:.2f}, Skewness={b_skewness:.2f}")'''
-			moments_ = [[b_mean,g_mean,r_mean],[b_std, g_std, r_std],[b_skew, g_skew, r_skew]]
+			moments_ = [[b_mean,g_mean,r_mean],[b_std, g_std, r_std],[b_skew, g_skew, r_skew],[b_kurt, g_kurt, r_kurt]]
 			for mKey,vals_ in zip(list(poolData_.keys()),moments_):
 				for v in vals_:
 					poolData_[mKey].append(v)
@@ -62,39 +66,30 @@ for f in files_:
 					"std":b_std,
 					"skew":b_skew,
 					"kurt":b_kurt,
-					"mean_norm":None,
-					"std_norm":None,
-					"skew_norm":None,
-					"kurt_norm":None
+					"mean_norm":None,"std_norm":None,"skew_norm":None,"kurt_norm":None
 				},
 				"g_channel":{
 					"mean":g_mean,
 					"std":g_std,
 					"skew":g_skew,
 					"kurt":g_kurt,
-					"mean_norm":None,
-					"std_norm":None,
-					"skew_norm":None,
-					"kurt_norm":None
+					"mean_norm":None,"std_norm":None,"skew_norm":None,"kurt_norm":None
 				},
 				"r_channel":{
 					"mean":r_mean,
 					"std":r_std,
 					"skew":r_skew,
 					"kurt":r_kurt,
-					"mean_norm":None,
-					"std_norm":None,
-					"skew_norm":None,
-					"kurt_norm":None
+					"mean_norm":None,"std_norm":None,"skew_norm":None,"kurt_norm":None
 				},
 				"plot":{"coords":[],"color":[]},
-				"path":impath,
+				"path":None,
 				"group":None,
 				"source_date_modified":date_modified
 			}
 		except Exception as e:
 			print(e)
-			pass
+			continue
 ######################################################################################
 min_mean = min(poolData_["mean"])
 max_mean = max(poolData_["mean"])
@@ -102,27 +97,36 @@ min_std = min(poolData_["std"])
 max_std = max(poolData_["std"])
 min_skew = min(poolData_["skew"])
 max_skew = max(poolData_["skew"])
+min_kurt = min(poolData_["kurt"])
+max_kurt = max(poolData_["kurt"])
 ######################################################################################
-for isKey,isItem in imageStats_.items():
+for imKey,imObj in imageStats_.items():
 	for channelKey in ["b_channel","g_channel","r_channel"]:
-		channelStats = isItem[channelKey]
+		channelStats = imObj[channelKey]
+		norm_mean = normailize_val(channelStats["mean"],min_kurt,max_kurt)
 		norm_std = normailize_val(channelStats["std"],min_std,max_std)
 		norm_skew = normailize_val(channelStats["skew"],min_skew,max_skew)
-		isItem[channelKey]["mean_norm"] = normailize_val(channelStats["mean"],min_mean,max_mean)
-		isItem[channelKey]["std_norm"] = normailize_val(channelStats["std"],min_std,max_std)
-		isItem[channelKey]["skew_norm"] = normailize_val(channelStats["skew"],min_skew,max_skew)
+		norm_kurt = normailize_val(channelStats["kurt"],min_kurt,max_kurt)
+		imObj[channelKey]["mean_norm"] = normailize_val(channelStats["mean"],min_mean,max_mean)
+		imObj[channelKey]["std_norm"] = normailize_val(channelStats["std"],min_std,max_std)
+		imObj[channelKey]["skew_norm"] = normailize_val(channelStats["skew"],min_skew,max_skew)
+		imObj[channelKey]["kurt_norm"] = normailize_val(channelStats["kurt"],min_kurt,max_kurt)
 ######################################################################################
-for isKey,isItem in imageStats_.items():
-	b_channel = isItem["b_channel"]
-	g_channel = isItem["g_channel"]
-	r_channel = isItem["r_channel"]
-	pt1 = [b_channel["mean_norm"],b_channel["std_norm"],b_channel["skew_norm"]]
-	pt2 = [g_channel["mean_norm"],g_channel["std_norm"],g_channel["skew_norm"]]
-	pt3 = [r_channel["mean_norm"],r_channel["std_norm"],r_channel["skew_norm"]]
-	isItem["plot"] = calc_midpoint(pt1,pt2,pt3)
-	isItem["color"] = [r_channel["mean"],g_channel["mean"],b_channel["mean"]]
+for imKey,imObj in imageStats_.items():
+	b_channel = imObj["b_channel"]
+	g_channel = imObj["g_channel"]
+	r_channel = imObj["r_channel"]
+	pt1 = [r_channel["mean_norm"],b_channel["mean_norm"],g_channel["mean_norm"]]
+	pt2 = [r_channel["std_norm"],b_channel["std_norm"],g_channel["std_norm"]]
+	pt3 = [r_channel["kurt_norm"],b_channel["kurt_norm"],g_channel["kurt_norm"]]
+	pt4 = [r_channel["skew_norm"],b_channel["skew_norm"],g_channel["skew_norm"]]
+	imObj["plot"] = calc_midpoint(pt1,pt2,pt3,pt4)
+	imObj["rgb"] = [r_channel["mean"],g_channel["mean"],b_channel["mean"]]
+	imObj["hex"] = rgb_to_hex((r_channel["mean"],g_channel["mean"],b_channel["mean"]))
+
 ######################################################################################
 with open(str(
 	"%s%s" % (r"02_output/","imageStats.json")
 	), "w", encoding='utf-8') as json_manifest:
 	json_manifest.write(json.dumps(imageStats_, indent=4, ensure_ascii=False))
+print("DONE")
